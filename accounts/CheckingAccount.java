aa834ee1c00eaa0c0d4b5f9aa34bce423229b174
Shilenko Ebgeniy <56047681+evgeniyshilenko@users.noreply.github.com>
2021-08-15 23:00:41 +0300

    Add files via upload

---
+++ accounts/CheckingAccount.java
@@ -0,0 +1,16 @@
+package com.gromcode.lesson8.accounts;
+
+public class CheckingAccount extends Account {
+    int limitOfExpences;
+
+    public CheckingAccount(String bankName, String ownerName, int moneyAmount, int limitOfExpences) {
+        super(bankName, ownerName, moneyAmount);
+        this.limitOfExpences = limitOfExpences;
+    }
+
+    public void withdraw(int amount) {
+        if (amount > limitOfExpences)
+            return;
+        moneyAmount -= amount;
+    }
+}
---
+++ accounts/MainAccount.java
@@ -0,0 +1,7 @@
+package com.gromcode.lesson8.accounts;
+
+public class MainAccount extends CheckingAccount {
+    public MainAccount(String bankName, String ownerName, int moneyAmount, int limitOfExpences) {
+        super(bankName, ownerName, moneyAmount, limitOfExpences);
+    }
+}
