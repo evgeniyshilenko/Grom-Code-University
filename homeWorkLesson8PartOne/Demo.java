4e26f8f8d09897e915febca6e11dc9f309dce418
Shilenko Ebgeniy <56047681+evgeniyshilenko@users.noreply.github.com>
2021-08-16 19:50:58 +0300

    Update Demo.java

--- homeWorkLesson8PartOne/Demo.java
+++ homeWorkLesson8PartOne/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.check(arr));
+        adder.check(arr);
         adder.add(5, 5);
     }
 }
