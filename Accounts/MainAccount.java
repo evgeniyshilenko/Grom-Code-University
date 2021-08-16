bd5175a0fd023a45d162ad4ff92747e1ccffd567
Shilenko Ebgeniy <56047681+evgeniyshilenko@users.noreply.github.com>
2021-08-16 20:57:33 +0300

    Delete Accounts directory

--- Accounts/MainAccount.java
+++
@@ -1,7 +0,0 @@
-package com.gromcode.lesson8.Accounts;
-
-public class MainAccount extends CheckingAccount {
-    public MainAccount(String bankName, String ownerName, int moneyAmount, int limitOfExpences) {
-        super(bankName, ownerName, moneyAmount, limitOfExpences);
-    }
-}
--- Accounts/SecondaryAccount.java
+++
@@ -1,7 +0,0 @@
-package com.gromcode.lesson8.Accounts;
-
-public class SecondaryAccount extends CheckingAccount {
-    public SecondaryAccount(String bankName, String ownerName, int moneyAmount, int limitOfExpences) {
-        super(bankName, ownerName, moneyAmount, limitOfExpences);
-    }
-}
