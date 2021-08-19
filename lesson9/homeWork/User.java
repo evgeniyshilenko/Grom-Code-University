5ef297b734ec5f178f2db8ec8d28cc276f4508ad
Shilenko Ebgeniy <56047681+evgeniyshilenko@users.noreply.github.com>
2021-08-19 22:22:45 +0300

    Add files via upload

---
+++ lesson9/homeWork/User.java
@@ -0,0 +1,13 @@
+package com.gromcode.lesson9.homeWork;
+
+public class User {
+    private long id;
+    private String name;
+    private String sessionID;
+
+    public User(long id, String name, String sessionID) {
+        this.id = id;
+        this.name = name;
+        this.sessionID = sessionID;
+    }
+}
